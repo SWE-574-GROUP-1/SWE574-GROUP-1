3ec1d2327b9f776b76b82d2634ec00cbb2b77c73
Ali Kenan Yağmur <ali.yagmur@ozan.com>
2023-03-13 11:09:06 +0300

    Revert .env path change

--- SWE573_Term_Project/LinkMe/settings.py
+++ SWE573_Term_Project/LinkMe/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
 # reading .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
